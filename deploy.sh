a62963b6dce544e76327ab46f735bd1d779943ad
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2026-06-21 18:06:52 -0500

    add cd, bash AND-list to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -euo pipefail
-git pull
-uv sync
-uv run manage.py migrate
-uv run manage.py collectstatic --noinput
-sudo systemctl restart unsphere
+cd /home/maxwell/unsphere/ && git pull && uv sync && uv run manage.py migrate && uv run manage.py collectstatic --noinput && sudo systemctl restart unsphere
