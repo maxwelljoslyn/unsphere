24b894b8a83c8ff1e7feb6362ebe0d931a287842
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2026-07-06 23:11:52 -0500

    display version number alongside Git commit
    Close #4

--- src/unsphere/settings.py
+++ src/unsphere/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
                 "achievements.context_processors.nav_achievements",
                 "achievements.context_processors.pending_achievements",
                 "gems.context_processors.nav_gems",
-                "unsphere.context_processors.git_commit",
+                "unsphere.context_processors.build_info",
             ],
         },
     },
